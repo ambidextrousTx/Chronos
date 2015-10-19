dc6c1b138a15b812e67d8fcdb964716df2829e79
ambidextrousTx <ravisinha@my.unt.edu>
2015-10-19 13:48:57 -0500

    PEP-8 modifications

--- src/project.py
+++ src/project.py
@@ -3,13 +3,17 @@ Represents a project
 """
 
 class Project(object):
+    ''' Represents a project, with some basic characteristics. Just the name
+    for now. More to be added later '''
     def __init__(self, name):
-        self.name = name
+        self._name = name
 
     def get_name(self):
+        ''' Getter for the private name '''
         return self._name
 
-    def set_name(self, n):
-        self._name = n
+    def set_name(self, new_name):
+        ''' Setter for the private name '''
+        self._name = new_name
 
     name = property(get_name, set_name)
--- src/subproject.py
+++ src/subproject.py
@@ -3,14 +3,17 @@ Each Project can have multiple SubProjects
 """
 
 class SubProject(object):
+    ''' A sub project associated with a project '''
     def __init__(self, name):
-        self.name = name
+        self._name = name
 
     def get_name(self):
+        ''' Getter for the name '''
         return self._name
 
-    def set_name(self, n):
-        self._name = n
+    def set_name(self, new_name):
+        ''' Setter for the name '''
+        self._name = new_name
 
     name = property(get_name, set_name)
 
